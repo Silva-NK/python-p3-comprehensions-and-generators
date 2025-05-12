dfd8a42e4dd337e82b22fe9a1b398867f384749a
Silva-NK <kiagonsuzanne@gmail.com>
2025-05-12 23:41:22 +0300

    Completes list comprehensions as per instructions.

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums = [num for num in num_list if num % 2 == 0]
+    return even_nums
+print(return_evens([0, 3, 8, 15, 24, 35, 48, 63, 80, 99]))
+
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentences = [f"{sentence}!" for sentence in sentence_list]
+    return new_sentences
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
